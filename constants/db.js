3f8fc98a3c09d230d8ca5258c281d9f68fd564e1
Leon Gaban <leongaban@gmail.com>
2024-02-19 16:59:36 -0600

    moved all db vars to .env

--- constants/db.js
+++ constants/db.js
@@ -1,13 +1,48 @@
 import knex from 'knex'
+import { config as dotenvConfig } from 'dotenv'
+dotenvConfig()
+
+const currentENV = process.env.ENVIRONMENT
+console.log('currentENV', currentENV)
+let host
+let port
+let user
+let password
+let database
+
+if (currentENV === 'localhost') {
+  console.log('WE ARE IN LOCALHOST')
+  const {
+    LOCAL_HOST,
+    LOCAL_DB_PORT,
+    LOCAL_USER,
+    LOCAL_DB_NAME,
+    LOCAL_DB_PASS,
+  } = process.env
+  host = LOCAL_HOST
+  port = LOCAL_DB_PORT
+  user = LOCAL_USER
+  database = LOCAL_DB_NAME
+  password = LOCAL_DB_PASS
+} else if (currentENV === 'production') {
+  console.log('WE ARE IN PRODUCTION')
+  const { PROD_DB_NAME, PROD_DB_PASS, PROD_PORT, PROD_USER, INTERNAL_DB_URL } =
+    process.env
+  host = INTERNAL_DB_URL
+  port = PROD_PORT
+  user = PROD_USER
+  database = PROD_DB_NAME
+  password = PROD_DB_PASS
+}
 
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    port: 5432,
-    user: 'leongaban',
-    password: '',
-    database: 'moon-portfolio',
+    host,
+    port,
+    user,
+    password,
+    database,
   },
 })
 
